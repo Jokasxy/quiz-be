c84164d2229d69490d86f3ca2bfcfcc47588baa8
Jokasxy <josip_1997@live.com>
2020-12-30 23:46:48 +0100

    Answer, Question, Feedback and Quiz lists added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { Keystone } = require('@keystonejs/keystone');
 const { PasswordAuthStrategy } = require('@keystonejs/auth-password');
-const { Text, Checkbox, Password } = require('@keystonejs/fields');
+const { Text, Checkbox, Password, Url, Select, Relationship } = require('@keystonejs/fields');
 const { GraphQLApp } = require('@keystonejs/app-graphql');
 const { AdminUIApp } = require('@keystonejs/app-admin-ui');
 const initialiseData = require('./initial-data');
@@ -44,10 +44,14 @@ const access = { userIsAdmin, userOwnsItem, userIsAdminOrOwner };
 
 keystone.createList('User', {
   fields: {
-    name: { type: Text },
+    name: {
+      type: Text,
+      isRequired: true,
+    },
     email: {
       type: Text,
       isUnique: true,
+      isRequired: true,
     },
     isAdmin: {
       type: Checkbox,
@@ -59,11 +63,132 @@ keystone.createList('User', {
     },
     password: {
       type: Password,
+      isRequired: true,
     },
   },
   // List-level access controls
   access: {
     read: access.userIsAdminOrOwner,
+    update: access.userIsAdminOrOwner,
+    create: access.userIsAdmin,
+    delete: access.userIsAdmin,
+    auth: true,
+  },
+});
+
+keystone.createList('Answer', {
+  fields: {
+    name: {
+      type: Text,
+      isRequired: true,
+    },
+    description: {
+      type: Text,
+    },
+  },
+  // List-level access controls
+  access: {
+    read: true,
+    update: access.userIsAdminOrOwner,
+    create: access.userIsAdmin,
+    delete: access.userIsAdmin,
+    auth: true,
+  },
+});
+
+keystone.createList('Question', {
+  fields: {
+    name: {
+      type: Text,
+    },
+    image: {
+      type: Url,
+    },
+    description: {
+      type: Text,
+      isRequired: true,
+    },
+    answers: {
+      type: Relationship,
+      ref: 'Answer',
+      many: true,
+    },
+    correct: {
+      type: Relationship,
+      ref: 'Answer',
+    },
+  },
+  // List-level access controls
+  access: {
+    read: true,
+    update: access.userIsAdminOrOwner,
+    create: access.userIsAdmin,
+    delete: access.userIsAdmin,
+    auth: true,
+  },
+});
+
+keystone.createList('Feedback', {
+  fields: {
+    name: {
+      type: Text,
+    },
+    image: {
+      type: Url,
+    },
+    description: {
+      type: Text,
+      isRequired: true,
+    },
+    category: {
+      type: Select,
+      options: 'terrible, bad, ok, good, excellent',
+      isRequired: true,
+    },
+  },
+  // List-level access controls
+  access: {
+    read: true,
+    update: access.userIsAdminOrOwner,
+    create: access.userIsAdmin,
+    delete: access.userIsAdmin,
+    auth: true,
+  },
+});
+
+keystone.createList('Quiz', {
+  fields: {
+    name: {
+      type: Text,
+      isRequired: true,
+    },
+    image: {
+      type: Url,
+    },
+    description: {
+      type: Text,
+    },
+    category: {
+      type: Select,
+      options: 'study, fun',
+      isRequired: true,
+    },
+    questions: {
+      type: Relationship,
+      ref: 'Question',
+      many: true,
+      isRequired: true,
+    },
+    feedback: {
+      type: Relationship,
+      ref: 'Feedback',
+      many: true,
+      isRequired: true,
+    },
+  },
+  // List-level access controls
+  access: {
+    read: true,
     update: access.userIsAdminOrOwner,
     create: access.userIsAdmin,
     delete: access.userIsAdmin,
